b8ee198e5e8f8ef8c5970bb96637fff70b0ac15c
Aladan Lazo Espinoza <faridlazo1921@gmail.com>
2026-01-28 09:59:48 -0500

    Update CourseMaterials.jsx

--- src/pages/instructor/CourseMaterials.jsx
+++ src/pages/instructor/CourseMaterials.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { Plus, Trash2, ArrowLeft } from 'lucide-react';
-import MaterialService from '../../services/materialService';
+import MaterialService from '../../services/MaterialService.js';
 
 const CourseMaterials = () => {
     const { courseId, sessionId } = useParams();
